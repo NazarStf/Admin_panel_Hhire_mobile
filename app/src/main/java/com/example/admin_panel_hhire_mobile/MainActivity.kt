9e928baff74d983c579178e945586211432fb634
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-22 15:53:43 +0200

    basic navigation, not functional

--- app/src/main/java/com/example/admin_panel_hhire_mobile/MainActivity.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.admin_panel_hhire_mobile.ui.posts.PostsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, PostsFragment())
+                .commit()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
