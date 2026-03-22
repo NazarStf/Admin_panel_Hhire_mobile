c6fbd5d6572741b10ddaab1675b5904ef5f90afe
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-22 12:20:39 +0200

    binding in LoginActivity

--- app/src/main/java/com/example/admin_panel_hhire_mobile/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/ui/auth/LoginActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.admin_panel_hhire_mobile.R
+import com.example.admin_panel_hhire_mobile.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.etLogin
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
