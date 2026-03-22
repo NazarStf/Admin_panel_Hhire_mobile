d69d0c050a147826601cf677e48dfd0be92dfaa9
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-22 22:09:05 +0200

    basic post trying to add chips

--- app/src/main/java/com/example/admin_panel_hhire_mobile/data/repository/PostRepository.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/data/repository/PostRepository.kt
@@ -24,7 +24,7 @@ object PostRepository {
         Post(3,  2, "TITLE2", "Потрібен електрик", System.currentTimeMillis(), PostStatus.ACTIVE, tags = listOf("електрика")),
         Post(3, 3, "3","Заміна труб", System.currentTimeMillis(), PostStatus.BLOCKED, tags = listOf("сантехніка")),
         Post(4, 4,"4", "Дизайн квартири", System.currentTimeMillis(), PostStatus.ACTIVE, tags = listOf("дизайн")),
-        Post(5, 5, "TITLE5","Ремонт даху", System.currentTimeMillis(), PostStatus.ACTIVE, tags = listOf("будівництво")),
+        Post(5, 5, "TITLE5","Ремонт даху", System.currentTimeMillis(), PostStatus.ACTIVE, tags = listOf("будівництво","ZHYD","KO MA","SLOW")),
         Post(6, 6,"TITLE6", "Ремонт авто", System.currentTimeMillis(), PostStatus.ACTIVE, tags = listOf("авто"))
     )
 
