c953f6b1eef3b7ae046c259ae7179479c2a3ea83
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-22 15:58:11 +0200

    basic navigation, functional

--- app/src/main/java/com/example/admin_panel_hhire_mobile/MainActivity.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.admin_panel_hhire_mobile.ui.posts.PostsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.admin_panel_hhire_mobile.ui.users.UsersFragment
+import com.example.admin_panel_hhire_mobile.ui.settings.SettingsFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +25,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnItemSelectedListener { item ->
+            val selectedFragment = when(item.itemId) {
+                R.id.nav_posts -> PostsFragment()
+                R.id.nav_users -> UsersFragment()
+                R.id.nav_settings -> SettingsFragment()
+                else -> PostsFragment()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit()
+
+            true
         }
     }
 }
