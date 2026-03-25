4513971790fa1589b9330fe5902bba46b1632020
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-25 09:37:37 +0200

    get post in PostDetailsFragment

--- app/src/main/java/com/example/admin_panel_hhire_mobile/ui/posts/PostDetailsFragment.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/ui/posts/PostDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.admin_panel_hhire_mobile.R
+import com.example.admin_panel_hhire_mobile.data.repository.PostRepository
 
 class PostDetailsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +16,16 @@ class PostDetailsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_post_details, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val postId = arguments?.getInt("postId") ?: return
+        val post = PostRepository.getPostById(postId)
+    }
 }
