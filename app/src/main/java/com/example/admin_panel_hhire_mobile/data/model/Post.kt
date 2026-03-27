7def996b6dcc00983726f3e7516ebc18cb346580
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-27 22:02:08 +0200

    fix from 7 phase

--- app/src/main/java/com/example/admin_panel_hhire_mobile/data/model/Post.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/data/model/Post.kt
@@ -3,9 +3,11 @@ package com.example.admin_panel_hhire_mobile.data.model
 data class Post(
     val id: Int,
     val authorId: Int,
+    var title: String,
     var content: String,
     val createdDate: Long,
     var status: PostStatus,
+    val editedDate: Long,
     var editDate: Long? = null,
     var photoLinks: List<String> = emptyList(),
     var tags: List<String> = emptyList()
