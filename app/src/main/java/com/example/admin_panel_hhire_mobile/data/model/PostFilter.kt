a1337a381d493fd4c8f88bac64f330a572dd683a
NazarStf <nazarii.stefanyshyn@kpk-lp.com.ua>
2026-03-31 19:06:29 +0300

    Working filter, reworked filtering status logic

---
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/data/model/PostFilter.kt
@@ -0,0 +1,8 @@
+package com.example.admin_panel_hhire_mobile.data.model
+
+enum class PostFilter {
+    ALL,
+    ACTIVE,
+    BLOCKED,
+    MARKED
+}
--- app/src/main/java/com/example/admin_panel_hhire_mobile/ui/posts/PostsFragment.kt
+++ app/src/main/java/com/example/admin_panel_hhire_mobile/ui/posts/PostsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.admin_panel_hhire_mobile.databinding.FragmentPostsBinding
 import com.example.admin_panel_hhire_mobile.data.model.Post
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.admin_panel_hhire_mobile.R
 import com.example.admin_panel_hhire_mobile.data.model.PostStatus
 import androidx.core.widget.addTextChangedListener
+import com.example.admin_panel_hhire_mobile.data.model.PostFilter
 
 class PostsFragment : Fragment() {
     private var _binding: FragmentPostsBinding? = null
@@ -19,7 +21,7 @@ class PostsFragment : Fragment() {
     private lateinit var adapter: PostAdapter
     private var allPosts = listOf<Post>()
     private var filteredPosts = listOf<Post>()
-    private var currentStatus = "ALL"
+    private var currentFilter = PostFilter.ALL
 
 
     override fun onCreateView(
@@ -36,7 +38,7 @@ class PostsFragment : Fragment() {
         filteredPosts = allPosts
         binding.etSearch.addTextChangedListener {
             val query = it.toString()
-            filterPosts(query, currentStatus)
+            filterPosts(query, currentFilter)
         }
 
         adapter = PostAdapter(filteredPosts) { post ->
@@ -52,11 +54,29 @@ class PostsFragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+        binding.filterIcon.setOnClickListener { view ->
+            val popup = PopupMenu(requireContext(), view)
+            popup.inflate(R.menu.filter_menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                currentFilter =  when (item.itemId) {
+                    R.id.all -> PostFilter.ALL
+                    R.id.marked -> PostFilter.MARKED
+                    R.id.blocked -> PostFilter.BLOCKED
+                    R.id.active -> PostFilter.ACTIVE
+                    else -> PostFilter.ALL
+                }
+                filterPosts(binding.etSearch.text.toString(), currentFilter)
+                true
+            }
+
+            popup.show()
+        }
         binding.recyclerViewPosts.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewPosts.adapter = adapter
 
     }
-    private fun filterPosts(query: String, status: String) {
+    private fun filterPosts(query: String, status: PostFilter) {
 
         filteredPosts = allPosts.filter { post ->
 
@@ -66,15 +86,15 @@ class PostsFragment : Fragment() {
                 post.content.contains(query, true) ||
                         (author?.name?.contains(query, true) == true)
 
-            val matchesStatus = when (status) {
-                "ALL" -> true
-                "ACTIVE" -> post.status == PostStatus.ACTIVE
-                "BLOCKED" -> post.status == PostStatus.BLOCKED
-                "MARKED" -> post.isMarked
+            val matchesFilter = when (status) {
+                PostFilter.ALL -> true
+                PostFilter.ACTIVE -> post.status == PostStatus.ACTIVE
+                PostFilter.BLOCKED -> post.status == PostStatus.BLOCKED
+                PostFilter.MARKED -> post.isMarked
                 else -> true
             }
 
-            matchesQuery && matchesStatus
+            matchesQuery && matchesFilter
         }
 
         adapter.updateList(filteredPosts)
